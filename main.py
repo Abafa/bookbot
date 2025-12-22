069ea7d697ca6102d9847e5b57c62edc7aade00f
Abafa <flo.beg@free.fr>
2025-12-22 03:19:25 +0100

    Now counting words !

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 def get_book_text (filepath) :
     with open(filepath) as f :
         return f.read()
+    
+def split_text (file) :
+    return file.split()
 
 def main () :
     text = get_book_text("books/frankenstein.txt")
-    print (text)
+    num_words = len(split_text(text))
+    print (f"Found {num_words} total words")
     return
 
 main()
