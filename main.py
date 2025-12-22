946e7134cdba60c792691e15cd5e8c03fbc9889e
Abafa <flo.beg@free.fr>
2025-12-22 13:45:46 +0100

    starting of some kind of sorting

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ def get_book_text (filepath) :
         return f.read()
     
 from stats import split_text
+from stats import count_character
 
 def main () :
     text = get_book_text("books/frankenstein.txt")
     num_words = len(split_text(text))
     print (f"Found {num_words} total words")
+    num_characters = count_character(text)
+    print(num_characters)
+
     return
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,16 @@
 def split_text (file) :
     return file.split()
+
+def count_character (file) : 
+
+    characters = {}
+    lowered_file = file.lower()  # prend file et lower() tout.
+
+
+    for i in lowered_file :      # itere sur toute la string et compare si :
+        if i in characters :     # si lettre checkée présente dans characters => characters =+
+            characters[i] += 1
+        else :                   # sinon add le nouveau truc dans le dico et set le a 1 
+            characters[i] = 1 
+    
+    return characters
